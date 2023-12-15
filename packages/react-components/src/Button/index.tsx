0920fc2d207ed5468cf9c817fe51c3f62e39ed04
junhobaik <junhobaik@gmail.com>
2023-12-15 22:53:16 +0900

    feat: react-components 기본 구조 생성

---
+++ packages/react-components/src/Button/index.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps {
+  children?: React.ReactNode;
+}
+
+const Button = (props: ButtonProps) => {
+  const { children } = props;
+  return <button>{children}</button>;
+};
+
+export default Button;
