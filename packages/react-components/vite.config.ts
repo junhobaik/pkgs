532cb10f1875dfa95a92cb16eeaf777c3adee5af
junhobaik <junhobaik@gmail.com>
2023-12-23 21:57:01 +0900

    style: lint

--- packages/react-components/vite.config.ts
+++ packages/react-components/vite.config.ts
@@ -1,8 +1,7 @@
-import {resolve} from "path";
-import {defineConfig} from "vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import dts from 'vite-plugin-dts';
-
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
