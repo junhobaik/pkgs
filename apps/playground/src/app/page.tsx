5f2255a2682cce9d959a55b968577f3d4015dda1
junhobaik <junhobaik@gmail.com>
2024-01-02 20:54:03 +0900

    feat: next ThemeProvider 추가

--- apps/playground/src/app/page.tsx
+++ apps/playground/src/app/page.tsx
@@ -1,12 +1,19 @@
+import ThemeSwitcher from '../components/ThemeSwitcher';
 import { Button } from 'ui';
 import 'ui/css';
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <Button>test1</Button>
-        <Button>test2</Button>
+      <div className="buttons">
+        <Button>UI Button</Button>
+        <ThemeSwitcher />
+        <button>Origin Button</button>
+
+        <div>
+          <div style={{ backgroundColor: 'var(--theme-color)', color: 'red' }}>--theme-color</div>
+          <div style={{ backgroundColor: 'var(--prefer-theme-color)', color: 'red' }}>--prefer-theme-color</div>
+        </div>
       </div>
     </main>
   );
