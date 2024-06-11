43455e3227b8774b1ee62cf230bf0a3c0a8258db
junhobaik <junhobaik@gmail.com>
2024-06-11 15:15:14 +0900

    chore: storybook theme

---
+++ apps/storybook/.storybook/theme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'junhobaik/pkgs',
+  brandUrl: 'https://github.com/junhobaik/pkgs',
+  // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self',
+});
